551235e4ff3d729ba0e2d380e1375c98d6535287
João Batista <jbdionisio@hotmail.com.br>
2018-12-19 21:35:01 -0200

    First commit

---
+++ src/learningGit/Main.java
@@ -0,0 +1,11 @@
+package learningGit;
+
+public class Main {
+
+	//First repository and project in github
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
